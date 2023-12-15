1f615ab30341f78a3273b98132bcf2f1c98f8501
enuchsa <enuchsa@gmail.com>
2023-12-15 17:27:11 -0300

    escrevendo arquivos com python

--- arquivos_de_exercicios_descubra_o_python/Cap. 04/escreveArquivo_start.py
+++ arquivos_de_exercicios_descubra_o_python/Cap. 04/escreveArquivo_start.py
@@ -1,3 +1,21 @@
 #
 # Escrevendo arquivos com funções do Python
 #
+
+def escreveArquivo():
+    arquivo = open("NoveArquivo.txt", "w+")
+    
+    arquivo.write("Linha gerada com a funcao 'escreveArquivo' \r\n")
+    
+    arquivo.close()
+    
+#escreveArquivo()
+
+def alteraArquivo():
+    arquivo = open("NoveArquivo.txt", "a+")
+    
+    arquivo.write("Linha gerada com a funcao 'alteraArquivo' \r\n")
+    
+    arquivo.close()
+    
+alteraArquivo()
