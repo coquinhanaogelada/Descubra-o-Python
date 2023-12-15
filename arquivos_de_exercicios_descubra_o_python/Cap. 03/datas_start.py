4c173836d7ae6d62389d05a1a455e75708de90bf
enuchsa <enuchsa@gmail.com>
2023-12-15 15:40:16 -0300

    datas

--- arquivos_de_exercicios_descubra_o_python/Cap. 03/datas_start.py
+++ arquivos_de_exercicios_descubra_o_python/Cap. 03/datas_start.py
@@ -2,3 +2,22 @@
 # Arquivo com exemplos de manipulação de  datas
 #
 
+from datetime import date
+from datetime import time
+from datetime import datetime
+
+def manipulaDataHora():
+    hoje = date.today()
+    print("Hoje é: ", hoje)
+    print("Partes da data: ", hoje.day, hoje.month, hoje.year)
+    print("Número do dia da semana: ", hoje.weekday())
+    dias = ['dom', 'seg', 'ter', 'qua', 'qui', 'sex', 'sab']
+    print("Nome abreviado do dia da semana: ", dias[hoje.weekday()])
+    
+    data = datetime.now()
+    print("Date e hora: ", data)
+    
+    tempo = datetime.time(data)
+    print("Hora atual: ", tempo)
+    
+manipulaDataHora()
