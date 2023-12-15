63df0a5a29d02a5d15fcc8be5a5e4adddf4772af
enuchsa <enuchsa@gmail.com>
2023-12-15 18:18:55 -0300

    manipulando arquivos com python e as bibliotecas os, shutil e path

--- arquivos_de_exercicios_descubra_o_python/Cap. 04/manipulaArquivos_start.py
+++ arquivos_de_exercicios_descubra_o_python/Cap. 04/manipulaArquivos_start.py
@@ -5,3 +5,18 @@ import os
 from os import path
 import shutil
 
+def copiarArquivo():
+    if path.exists('NoveArquivo.txt'):
+        pathAtual = path.realpath('NoveArquivo.txt')
+        novoPath = pathAtual + '.bkp'
+        shutil.copy(pathAtual, novoPath)
+        
+        shutil.copystat(pathAtual, novoPath)
+        
+# copiarArquivo()
+
+def renomearArquivo():
+    if path.exists('NoveArquivo.txt.bkp'):
+        os.rename("NoveArquivo.txt.bkp", "ArquivoAlterado2.txt")
+        
+renomearArquivo()
