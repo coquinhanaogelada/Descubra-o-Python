0e0c79847d8d0595adc54abd83175645ad66e7d5
enuchsa <enuchsa@gmail.com>
2023-12-15 17:54:51 -0300

    usando biblioteca path

--- arquivos_de_exercicios_descubra_o_python/Cap. 04/pathArquivos_start.py
+++ arquivos_de_exercicios_descubra_o_python/Cap. 04/pathArquivos_start.py
@@ -4,3 +4,20 @@
 
 from os  import path
 import time
+
+def dadosArquivos():
+    arquivoExiste = path.exists('NoveArquivo.txt')
+    eDiretorio = path.isdir('NoveArquivo.txt')
+    pathArquivo = path.realpath('NoveArquivo.txt')
+    pathRelativo = path.relpath('NoveArquivo.txt')
+    dataCriacao = time.ctime(path.getctime('NoveArquivo.txt'))
+    dataModificacao = time.ctime(path.getmtime('NoveArquivo.txt'))
+    
+    print(arquivoExiste)
+    print(eDiretorio)
+    print(pathArquivo)
+    print(pathRelativo)
+    print(dataCriacao)
+    print(dataModificacao)
+    
+dadosArquivos()
