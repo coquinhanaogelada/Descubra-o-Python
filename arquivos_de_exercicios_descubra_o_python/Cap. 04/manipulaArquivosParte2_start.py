ad99f5decf26eea7da983afe6a37b461ecfb47f6
enuchsa <enuchsa@gmail.com>
2023-12-15 18:34:07 -0300

    manipulando arquivos com python

--- arquivos_de_exercicios_descubra_o_python/Cap. 04/manipulaArquivosParte2_start.py
+++ arquivos_de_exercicios_descubra_o_python/Cap. 04/manipulaArquivosParte2_start.py
@@ -7,3 +7,18 @@ import shutil
 from shutil import make_archive
 from zipfile import ZipFile
 
+def criaZipModo1():
+    shutil.make_archive("ArquivoCompactado", "zip", "C:\\Users\\enuch\\Documents\\Cursos\\Descobrindo o python - LinkedIn\\Descubra-o-Python\\arquivos_de_exercicios_descubra_o_python\\Cap. 03")
+    
+# criaZipModo1()
+
+def criaZipModo2():
+    with ZipFile("ArquivoZipModo2.zip", "w") as novoZip:
+        novoZip.write("NoveArquivo.txt")
+        
+# criaZipModo2()
+        
+def deletaArquivo():
+    os.remove("ArquivoZipModo2.zip")
+    
+deletaArquivo()
