664647462eb214476516672ca31e150636be0a8a
enuchsa <enuchsa@gmail.com>
2023-12-15 14:47:27 -0300

    classes

--- arquivos_de_exercicios_descubra_o_python/Cap. 02/classes_start.py
+++ arquivos_de_exercicios_descubra_o_python/Cap. 02/classes_start.py
@@ -1,3 +1,25 @@
 #
 # Exemplo de como criar classes
 #
+
+class minhaClasse():
+    def __init__(self) -> None:
+        self.meuAtributo = "Passou pelo construtor!"
+    
+    def meuMetodo(self):
+        print("Passou pelo meuMetodo")
+        
+    def meuMetodo2(self, valor):
+        self.outroAtributo = valor
+        print(self.outroAtributo)
+        
+def criarObjeto():
+    meuObj = minhaClasse()
+    var1 = meuObj.meuAtributo
+    print(var1)
+    
+    meuObj.meuMetodo()
+    
+    meuObj.meuMetodo2("Executando um método")
+    
+criarObjeto()
