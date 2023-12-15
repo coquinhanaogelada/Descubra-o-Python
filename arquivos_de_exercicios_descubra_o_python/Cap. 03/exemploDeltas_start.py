bac19841ea71e8b1eb28a41b3ce619e93b5ecd64
enuchsa <enuchsa@gmail.com>
2023-12-15 16:36:22 -0300

    exemplos com delta

--- arquivos_de_exercicios_descubra_o_python/Cap. 03/exemploDeltas_start.py
+++ arquivos_de_exercicios_descubra_o_python/Cap. 03/exemploDeltas_start.py
@@ -6,3 +6,16 @@ from datetime import date
 from datetime import time
 from datetime import datetime
 from datetime import timedelta
+
+def quantosDiasFaltam(ano, mes, dia):
+    hoje = date.today()
+    dataProcurada = date(ano, mes, dia)
+    
+    qtosDias = dataProcurada - hoje
+    
+    mensagemRetorno = "Faltam " + str(qtosDias).replace("days, 0:00:00", "") + " dias para a data " + dataProcurada.strftime("%d/%m/%y")
+    
+    print(mensagemRetorno)
+    
+quantosDiasFaltam(2024, 1, 1)
+    
