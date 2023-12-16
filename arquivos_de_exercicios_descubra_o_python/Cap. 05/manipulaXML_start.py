eadd1246fe1e475e3b67d6d0ac37c69628751603
enuchsa <enuchsa@gmail.com>
2023-12-15 23:07:28 -0300

    manipulando xml

--- arquivos_de_exercicios_descubra_o_python/Cap. 05/manipulaXML_start.py
+++ arquivos_de_exercicios_descubra_o_python/Cap. 05/manipulaXML_start.py
@@ -3,3 +3,15 @@
 #
 
 import xml.dom.minidom
+
+def manipulaXML():
+    doc = xml.dom.minidom.parse('C:\\Users\\enuch\\Documents\\Cursos\Descobrindo o python - LinkedIn\\Descubra-o-Python\\arquivos_de_exercicios_descubra_o_python\\Cap. 05\\exemploXML.xml')
+    
+    print("Nome da primeira tag: ", str(doc.firstChild.tagName))
+    primeiroNome = doc.getElementsByTagName("firstname")
+    print(primeiroNome[0].firstChild.nodeValue)
+    
+    for skill in doc.getElementsByTagName("skill"):
+        print("Skill encontrado: ", skill.getAttribute("name"))
+        
+manipulaXML()
