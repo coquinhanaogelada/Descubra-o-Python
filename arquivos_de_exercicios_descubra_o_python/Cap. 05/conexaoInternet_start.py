9319b53148259e6fbecb2e557fe1acad4c81e968
enuchsa <enuchsa@gmail.com>
2023-12-15 21:53:22 -0300

    request com python e urllib.request

--- arquivos_de_exercicios_descubra_o_python/Cap. 05/conexaoInternet_start.py
+++ arquivos_de_exercicios_descubra_o_python/Cap. 05/conexaoInternet_start.py
@@ -4,5 +4,11 @@
 
 import urllib.request
 
+def conectaInternet():
+    objUrl = urllib.request.urlopen("http://www.google.com")
+    
+    if objUrl.getcode() == 200:
+        dados = objUrl.read()
+        print(dados)
 
-
+conectaInternet()
